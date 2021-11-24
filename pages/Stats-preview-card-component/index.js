09930418c1884fc1061cc449dc025969ac525ccc
Mostafa Samir <mostafasamer1999@hotmail.com>
2021-11-25 00:26:00 +0200

    Hot Fix Deploy issues

--- pages/Stats-preview-card-component/index.js
+++ pages/Stats-preview-card-component/index.js
@@ -8,7 +8,7 @@ const WebDesginChallenge = function () {
             <Head>
                 <link rel="icon" href="/images/order-summary-component/favicon-32x32.png" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Lexend+Deca&display=swap" rel="stylesheet" />
             </Head>
             <div className={styles.background}>
