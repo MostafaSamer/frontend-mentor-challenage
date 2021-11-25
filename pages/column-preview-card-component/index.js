1e71a36cf32233a817bfcaab633ffd021ddb3406
Mostafa Samir <mostafasamer1999@hotmail.com>
2021-11-26 01:17:43 +0200

    Finish Third

--- pages/column-preview-card-component/index.js
+++ pages/column-preview-card-component/index.js
@@ -7,39 +7,91 @@ const WebDesginChallenge = function () {
         <Fragment>
             <Head>
                 <link rel="icon" href="/images/column-preview-card-component/favicon-32x32.png" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@700&family=Lexend+Deca&display=swap" rel="stylesheet" />
             </Head>
             <div className={styles.background}>
                 <div className={styles.cardHolder}>
                     <div className={styles.card}>
-                        <div className={styles.cardHeader}>
-                            <img src="/images/column-preview-card-component/illustration-hero.svg"/>
-                        </div>
-                        <div className={styles.cardBody + " px-5 text-center"}>
-                            <div className="mt-5">
-                                <h3 className={styles.bodyHeader}>Order Summary</h3>
-                            </div>
-                            <div className="mt-3">
-                                <p className={styles.bodyPara}>You can now listen to miliones of songs, audiobooks and podcasts on any device anywhere you like!</p>
-                            </div>
-                            <div className={styles.bodyPlanCard + " mt-4"}>
-                                <div className="d-flex">
-                                    <div className="col p-0 d-flex justify-content-start">
-                                        <div className="col-auto pl-0"><img src="/images/column-preview-card-component/icon-music.svg"/></div>
-                                        <div className="col-auto p-0">
-                                            <div className={styles.bodyPlanName}>Annual Plan</div>
-                                            <div className={styles.bodyPlanPrice}>$59.99/year</div>
+                        <div className="row">
+                            <div className="col-md-4 col-12 p-0">
+                                <div className={styles.cardBody + " " + styles.cardBody1}>
+                                    <div className="row">
+                                        <div className={styles.bodyIcon + " mt-5"}>
+                                            <img src="/images/column-preview-card-component/icon-sedans.svg"/>
                                         </div>
                                     </div>
-                                    <div className={styles.bodyPlanChange + " col p-0 d-flex justify-content-end"}><a className="d-flex align-items-center" href="#">Change</a></div>
+                                    <div className="row">
+                                        <div className={styles.bodyHeader + " mt-5"}>SEDRNS</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className={styles.bodyParag + " mt-4 mb-5"}>
+                                            Choose a sedans for its
+                                            affordability and excellent
+                                            fuel economy. Ideal for
+                                            cruising in the city or on
+                                            your next road trip.
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className={styles.bodyButton + " mt-5 mb-4"}>
+                                            <button>Learn More</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div className={styles.bodyProceed + " mt-5"}>
-                                <button className="w-100">
-                                    Proceed to Payment
-                                </button>
+                            <div className="col-md-4 col-12 p-0">
+                                <div className={styles.cardBody + " " + styles.cardBody2}>
+                                    <div className="row">
+                                        <div className={styles.bodyIcon + " mt-5"}>
+                                            <img src="/images/column-preview-card-component/icon-suvs.svg"/>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className={styles.bodyHeader + " mt-5"}>SUVS</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className={styles.bodyParag + " mt-4 mb-5"}>
+                                            Take on suv for its spocious
+                                            interior, power, ond
+                                            versotilitY, Perfect for your
+                                            next fomily vocotion ond
+                                            off-rood odventures.
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className={styles.bodyButton + " mt-5 mb-4"}>
+                                            <button>Learn More</button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div className={styles.bodyCancel + " mt-4 mb-5"}>
-                                Cancel Order
+                            <div className="col-md-4 col-12 p-0">
+                                <div className={styles.cardBody + " " + styles.cardBody3}>
+                                    <div className="row">
+                                        <div className={styles.bodyIcon + " mt-5"}>
+                                            <img src="/images/column-preview-card-component/icon-luxury.svg"/>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className={styles.bodyHeader + " mt-5"}>LUXURV</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className={styles.bodyParag + " mt-4 mb-5"}>
+                                        Cruise in the best cor bronds
+                                        without the blooted prices.
+                                        EnjoY the enhonced comforf
+                                        of o luxury rentol ond orrive
+                                        in style
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className={styles.bodyButton + " mt-5 mb-4"}>
+                                            <button>Learn More</button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
