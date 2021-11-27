b9676d9083dc4369bf5fe1e6ed8259cd98dddef4
Mostafa Samir <mostafasamer1999@hotmail.com>
2021-11-27 19:58:58 +0200

    Start in the Fith Project (faq)

--- pages/faq-accordion-card/index.js
+++ pages/faq-accordion-card/index.js
@@ -10,6 +10,9 @@ const WebDesginChallenge = function () {
         <Fragment>
             <Head>
                 <link rel="icon" href="/images/faq-accordion-card/favicon-32x32.png" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap" rel="stylesheet" />
             </Head>
             <div className={styles.background}>
                 <div className={styles.cardHolder}>
@@ -23,19 +26,101 @@ const WebDesginChallenge = function () {
                                 </div>
                             </div>
                             <div className="col-md-6 col-12">
-                                <div className="row">FAQ</div>
-                                <div className="row">
+                                <div className={styles.bodyHeader + " row"}>
+                                    <h2>FAQ</h2>
+                                </div>
+                                <div className={styles.bodyDropdownHolder + " row py-3 m-0"}>
                                     <div className={styles.bodyDropdown + " w-100"} onClick={() => { SelectedIndexQuestion == 0? SetSelectedIndexQuestion(-1) : SetSelectedIndexQuestion(0) }}>
-                                        <div className={styles.bodyQuestion + " row"}>
-                                            <div className="col-auto">How many team member can I invite?</div>
-                                            <div className={" col-auto"}>
+                                        <div className={styles.bodyQuestion + " " + (SelectedIndexQuestion == 0? styles.bodyQuestionSelected : "") + " row"}>
+                                            <div className="col-auto p-0">How many team member can I invite?</div>
+                                            <div className={" col-auto p-0"}>
                                             <img className={(SelectedIndexQuestion == 0? styles.bodyQuestionArrowFiliped : "")} src="/images/faq-accordion-card/icon-arrow-down.svg"/>
                                             </div>
                                         </div>
-                                        {/* <div className={styles.bodyAnswer}>
+                                        <div className={(SelectedIndexQuestion == 0? styles.bodyAnswerShow : styles.bodyAnswerHide)}>
                                             No more than 2GB. All files in your account must fit your
                                             allotted storage space.
-                                        </div> */}
+                                        </div>
+                                    </div>
+                                </div>
+
+                                
+                                <div className={styles.bodyDropdownHolder + " row py-3 m-0"}>
+                                    <div className={styles.bodyDropdown + " w-100"} onClick={() => { SelectedIndexQuestion == 1? SetSelectedIndexQuestion(-1) : SetSelectedIndexQuestion(1) }}>
+                                        <div className={styles.bodyQuestion + " " + (SelectedIndexQuestion == 1? styles.bodyQuestionSelected : "") + " row"}>
+                                            <div className="col-auto p-0">What is the maximum file upload size?</div>
+                                            <div className={" col-auto p-0"}>
+                                            <img className={(SelectedIndexQuestion == 1? styles.bodyQuestionArrowFiliped : "")} src="/images/faq-accordion-card/icon-arrow-down.svg"/>
+                                            </div>
+                                        </div>
+                                        <div className={(SelectedIndexQuestion == 1? styles.bodyAnswerShow : styles.bodyAnswerHide)}>
+                                            No more than 2GB. All files in your account must fit your
+                                            allotted storage space.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                
+                                <div className={styles.bodyDropdownHolder + " row py-3 m-0"}>
+                                    <div className={styles.bodyDropdown + " w-100"} onClick={() => { SelectedIndexQuestion == 2? SetSelectedIndexQuestion(-1) : SetSelectedIndexQuestion(2) }}>
+                                        <div className={styles.bodyQuestion + " " + (SelectedIndexQuestion == 2? styles.bodyQuestionSelected : "") + " row"}>
+                                            <div className="col-auto p-0">How do I reset my password?</div>
+                                            <div className={" col-auto p-0"}>
+                                            <img className={(SelectedIndexQuestion == 2? styles.bodyQuestionArrowFiliped : "")} src="/images/faq-accordion-card/icon-arrow-down.svg"/>
+                                            </div>
+                                        </div>
+                                        <div className={(SelectedIndexQuestion == 2? styles.bodyAnswerShow : styles.bodyAnswerHide)}>
+                                            No more than 2GB. All files in your account must fit your
+                                            allotted storage space.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                
+                                <div className={styles.bodyDropdownHolder + " row py-3 m-0"}>
+                                    <div className={styles.bodyDropdown + " w-100"} onClick={() => { SelectedIndexQuestion == 3? SetSelectedIndexQuestion(-1) : SetSelectedIndexQuestion(3) }}>
+                                        <div className={styles.bodyQuestion + " " + (SelectedIndexQuestion == 3? styles.bodyQuestionSelected : "") + " row"}>
+                                            <div className="col-auto p-0">Can I cancle my subscribtion?</div>
+                                            <div className={" col-auto p-0"}>
+                                            <img className={(SelectedIndexQuestion == 3? styles.bodyQuestionArrowFiliped : "")} src="/images/faq-accordion-card/icon-arrow-down.svg"/>
+                                            </div>
+                                        </div>
+                                        <div className={(SelectedIndexQuestion == 3? styles.bodyAnswerShow : styles.bodyAnswerHide)}>
+                                            No more than 2GB. All files in your account must fit your
+                                            allotted storage space.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                
+                                <div className={styles.bodyDropdownHolder + " row py-3 m-0"}>
+                                    <div className={styles.bodyDropdown + " w-100"} onClick={() => { SelectedIndexQuestion == 4? SetSelectedIndexQuestion(-1) : SetSelectedIndexQuestion(4) }}>
+                                        <div className={styles.bodyQuestion + " " + (SelectedIndexQuestion == 4? styles.bodyQuestionSelected : "") + " row"}>
+                                            <div className="col-auto p-0">Do you provide additional support?</div>
+                                            <div className={" col-auto p-0"}>
+                                            <img className={(SelectedIndexQuestion == 4? styles.bodyQuestionArrowFiliped : "")} src="/images/faq-accordion-card/icon-arrow-down.svg"/>
+                                            </div>
+                                        </div>
+                                        <div className={(SelectedIndexQuestion == 4? styles.bodyAnswerShow : styles.bodyAnswerHide)}>
+                                            No more than 2GB. All files in your account must fit your
+                                            allotted storage space.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                
+                                <div className={styles.bodyDropdownHolder + " row py-3 m-0"}>
+                                    <div className={styles.bodyDropdown + " w-100"} onClick={() => { SelectedIndexQuestion == 5? SetSelectedIndexQuestion(-1) : SetSelectedIndexQuestion(5) }}>
+                                        <div className={styles.bodyQuestion + " " + (SelectedIndexQuestion == 5? styles.bodyQuestionSelected : "") + " row"}>
+                                            <div className="col-auto p-0">How many team member can I invite?</div>
+                                            <div className={" col-auto p-0"}>
+                                            <img className={(SelectedIndexQuestion == 5? styles.bodyQuestionArrowFiliped : "")} src="/images/faq-accordion-card/icon-arrow-down.svg"/>
+                                            </div>
+                                        </div>
+                                        <div className={(SelectedIndexQuestion == 5? styles.bodyAnswerShow : styles.bodyAnswerHide)}>
+                                            No more than 2GB. All files in your account must fit your
+                                            allotted storage space.
+                                        </div>
                                     </div>
                                 </div>
                             </div>
