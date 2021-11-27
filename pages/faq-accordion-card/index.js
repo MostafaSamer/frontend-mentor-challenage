7ca328fded5cb1b856fb7a34cc687049aea6d578
Mostafa Samir <mostafasamer1999@hotmail.com>
2021-11-27 12:54:00 +0200

    Start in the Fith Project (faq)

--- pages/faq-accordion-card/index.js
+++ pages/faq-accordion-card/index.js
@@ -1,8 +1,11 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import Head from 'next/head'
 import styles from '../../styles/faq-accordion-card.module.css'
 
 const WebDesginChallenge = function () {
+
+    const [SelectedIndexQuestion, SetSelectedIndexQuestion] = useState(-1);
+
     return (
         <Fragment>
             <Head>
@@ -11,17 +14,23 @@ const WebDesginChallenge = function () {
             <div className={styles.background}>
                 <div className={styles.cardHolder}>
                     <div className={styles.card}>
-                        <div className="row">
+                        <div className="row m-0">
                             <div className="col-md-6 col-12">
-                                Multi Image
+                                <div className={styles.imgHolder}>
+                                    <img width="720" src="/images/faq-accordion-card/bg-pattern-desktop.svg"/>
+                                    <img width="720" src="/images/faq-accordion-card/illustration-box-desktop.svg"/>
+                                    <img width="720" src="/images/faq-accordion-card/illustration-woman-online-desktop.svg"/>
+                                </div>
                             </div>
                             <div className="col-md-6 col-12">
                                 <div className="row">FAQ</div>
                                 <div className="row">
-                                    <div className={styles.bodyDropdown}>
+                                    <div className={styles.bodyDropdown + " w-100"} onClick={() => { SelectedIndexQuestion == 0? SetSelectedIndexQuestion(-1) : SetSelectedIndexQuestion(0) }}>
                                         <div className={styles.bodyQuestion + " row"}>
-                                            <div className="col">How many team member can I invite?</div>
-                                            <div className="col">Arrow</div>
+                                            <div className="col-auto">How many team member can I invite?</div>
+                                            <div className={" col-auto"}>
+                                            <img className={(SelectedIndexQuestion == 0? styles.bodyQuestionArrowFiliped : "")} src="/images/faq-accordion-card/icon-arrow-down.svg"/>
+                                            </div>
                                         </div>
                                         {/* <div className={styles.bodyAnswer}>
                                             No more than 2GB. All files in your account must fit your
