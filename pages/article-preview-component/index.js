4509b29e9df9eceb3c608881053fb7f028f4d200
Mostafa Samir <mostafasamer1999@hotmail.com>
2021-12-10 18:41:46 +0200

    WIP

--- pages/article-preview-component/index.js
+++ pages/article-preview-component/index.js
@@ -22,33 +22,38 @@ const WebDesginChallenge = function () {
                             </div>
                             <div className="col-md-7 col-12 p-0">
                                 <div className={styles.sectionHolder}>
-                                    <div className="row m-0">
-                                        <div className={styles.sectionHeader}>
-                                            <h4>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h4>
+                                    <div className={styles.sectionHolderRow + ' row m-0'}>
+                                        <div className="col-12 p-0 m-0 mb-3">
+                                            <div className={styles.sectionHeader}>
+                                                <h4>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h4>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="row m-0">
-                                        <div className={styles.sectionPara}>
-                                            <p className="m-0">
-                                                Ever been ln a room and felt like something was missing?
-                                                Perhaps it felt slightly bare and uninviting. I've got some
-                                                simple tips to help you make any room feel complete.
-                                            </p>
+                                        <div className="col-12 p-0 m-0 mb-3">
+                                            <div className={styles.sectionPara}>
+                                                <p className="m-0">
+                                                    Ever been ln a room and felt like something was missing?
+                                                    Perhaps it felt slightly bare and uninviting. I've got some
+                                                    simple tips to help you make any room feel complete.
+                                                </p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="row m-0">
-                                        <div className={styles.authSection + " mt-3"}>
-                                            <div className='row'>
-                                                <div className='col-3'>
-                                                    <div className={styles.authImageHolder}>
-                                                        <img src="/images/article-preview-component/avatar-michelle.jpg"/>
+                                        <div className="col-12 p-0 m-0">
+                                            <div className={styles.authSection + " mt-3"}>
+                                                <div className='row'>
+                                                    <div className='col-2'>
+                                                        <div className={styles.authImageHolder}>
+                                                            <img src="/images/article-preview-component/avatar-michelle.jpg"/>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div className='col-6'>
-                                                    INFO
-                                                </div>
-                                                <div className='col-3'>
-                                                    Share Button
+                                                    <div className='col-8'>
+                                                        <div className={styles.infoHeader}><h6>Michelle Appleton</h6></div>
+                                                        <div className={styles.infoDate}><p>28 Jun 2020</p></div>
+                                                    </div>
+                                                    <div className='col-2'>
+                                                        <div className={styles.infoShareImg + ' d-flex justify-content-center align-items-center'}>
+                                                            <img src="/images/article-preview-component/icon-share.svg" />
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
