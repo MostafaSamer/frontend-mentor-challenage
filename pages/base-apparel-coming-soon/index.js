8ba9147f622c8e23c288e5dd8c4763fd4501a69d
Mostafa Samir <mostafasamer1999@hotmail.com>
2021-12-27 15:57:33 +0200

    WIP

--- pages/base-apparel-coming-soon/index.js
+++ pages/base-apparel-coming-soon/index.js
@@ -1,14 +1,23 @@
 import { Fragment } from 'react'
 import Head from 'next/head'
-import styles from '../../styles/column-preview-card-component.module.css'
+import styles from '../../styles/base-apparel-coming-soon.module.css'
 
 const WebDesginChallenge = function () {
     return (
         <Fragment>
             <Head>
-                <link rel="icon" href="/images/column-preview-card-component/favicon-32x32.png" />
+                <link rel="icon" href="/images/base-apparel-coming-soon/favicon-32x32.png" />
             </Head>
             <div className={styles.background}>
+                <div className={styles.section + ' row m-0 h-100'}>
+                    <div className='col-7 p-0'>
+                    </div>
+                    <div className='col-5 p-0'>
+                        <div className={styles.ImageHolder + '  w-100'}>
+                            <img src="/images/base-apparel-coming-soon/hero-desktop.jpg"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Fragment>
     )
