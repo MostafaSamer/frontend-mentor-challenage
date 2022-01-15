b4e75d58a0287340d1803f805f0180fda795ca95
Mostafa Samir <mostafasamer1999@hotmail.com>
2022-01-16 00:03:35 +0200

    WIP

--- pages/time-tracking-dashboard/index.js
+++ pages/time-tracking-dashboard/index.js
@@ -13,18 +13,38 @@ const WebDesginChallenge = function () {
             </Head>
             <div className={styles.background}>
                 <div className={styles.cardHolder}>
-                    <div className={styles.card + " d-flex"}>
-                        <div className='row m-0'>
-                            <div className='col-md-3 col-12'>
-                                BIG CARDS
-                            </div>
-                            <div className='col-md-9 col-12'>
-                                <div className='row m-0'>
-                                    <div className='col-1'></div>
-                                    <div className='col-1'></div>
-                                    <div className='col-1'></div>
+                    <div className='row m-0'>
+                        <div className='col-md-3 col-12'>
+                            <div className={styles.bigCard}>
+                                <div className={styles.bigCardUpper + " row m-0 p-4"}>
+                                    <div className={styles.profileImgHolder}>
+                                        <img src="/images/time-tracking-dashboard/image-jeremy.png" />
+                                    </div>
+
+                                    <div className={styles.reportTxt + " row m-0"}>
+                                        <p className='m-0'>Report for</p>
+                                    </div>
+
+                                    <div className={styles.name + " row m-0"}>
+                                        <h1>Jeremy</h1> <br />
+                                        <h1>Robson</h1>
+                                    </div>
+                                </div>
+                                <div className={styles.bigCardBlow + " row m-0"}>
+                                    <div className={styles.BlowTime + " row m-0"}><p>Daily</p></div>
+                                    <div className={styles.BlowTime + " row m-0"}><p>Weekly</p></div>
+                                    <div className={styles.BlowTime + " row m-0"}><p>Monthly</p></div>
                                 </div>
                             </div>
+                        </div>
+                        <div className='col-md-3 col-12'>
+                            BIG CARDS
+                        </div>
+                        <div className='col-md-3 col-12'>
+                            BIG CARDS
+                        </div>
+                        <div className='col-md-3 col-12'>
+                            BIG CARDS
                         </div>
                     </div>
                 </div>
